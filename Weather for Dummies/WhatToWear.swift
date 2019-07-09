0567689f7ae15219892b99b3c79d4e83eb1e90c9
Matvey Kostukovsky <matvelius@gmail.com>
2019-07-09 00:45:12 -0500

    add WhatToWear

---
+++ Weather for Dummies/WhatToWear.swift
@@ -0,0 +1,31 @@
+//
+//  WhatToWear.swift
+//  Weather for Dummies
+//
+//  Created by Matvey on 7/8/19.
+//  Copyright © 2019 Matvey. All rights reserved.
+//
+
+import Foundation
+
+var dangerouslyHot = false
+var dangerouslyCold = false
+var calamityWarning = false
+
+var bringCoatOrUmbrella = false
+var waterproofShoes = false
+var wearHat = false
+var wearSweater = false
+var wearGloves = false
+
+var numberOfLayers = 1
+
+let hotOrCold = ["It is hot, ", "It is pretty warm, ", "Not too hot and not too cold, ", "It is chilly, ", "It is cold, ", "It is very cold, "]
+
+let precipitationStatus = ["dry, ", "humid, ", "foggy, ", "raining, ", "pouring, ", "snowing, ", "hailing, "]
+
+let windyOrNot = ["and not windy.", "and windy."]
+
+
+
+
